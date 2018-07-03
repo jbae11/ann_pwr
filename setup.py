ddd11edaea7e0e898fd032f486f8eafc24bcfb8a
jbae11@illinois.edu <jbae11@illinois.edu>
2018-07-03 13:51:51 -0400

    saltproc reactor

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 VERSION = '0.0.1'
 setup_kwargs = {
     "version": VERSION,
-    "description": 'UDB Reactor',
+    "description": 'Saltproc Reactor',
     "author": 'Jin Whan Bae',
     }
 
 if __name__ == '__main__':
     setup(
-        name='udb_reactor',
-        packages=["udb_reactor"],
+        name='saltproc_reactor',
+        packages=["saltproc_reactor"],
         **setup_kwargs
         )
