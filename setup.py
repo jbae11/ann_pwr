2fb593d272c2f30beca2310d0174e0693d3ecff4
Jin Whan Bae <jbae11@illinois.edu>
2019-01-31 14:04:14 -0500

    setup

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 VERSION = '0.0.1'
 setup_kwargs = {
     "version": VERSION,
-    "description": 'Saltproc Reactor',
+    "description": 'ANN LWR',
     "author": 'Jin Whan Bae',
     }
 
 if __name__ == '__main__':
     setup(
-        name='saltproc_reactor',
-        packages=["saltproc_reactor"],
+        name='ann_lwr',
+        packages=["ann_lwr"],
         **setup_kwargs
         )
